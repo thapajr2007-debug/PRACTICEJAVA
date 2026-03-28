68287125dd11ffe0db810fccfc22276e38b125b6
thapajr2007-debug <thapajr2007@gmail.com>
2026-03-28 14:21:01 +0545

    learning variables

---
+++ Day3.java
@@ -0,0 +1,11 @@
+public class Day3{
+   public static void main(String[] args){
+    // I am training hard for my java course - commment
+    /*dsplkfkmdfk msdfv mfmdksdfoiojojfjjhfvg  fdhgohgfohagjoh
+    gjosjdogjofdj ogfdj
+    fdojoj     This is a multiline comment. */
+     System.out.println("Hello rajan"); // we can put comment in side too.
+   } 
+}
+  
+
---
+++ code.java
@@ -0,0 +1,8 @@
+public class code {
+    public static void main(String[] args){
+        String firstname= ("Rajan");
+        String Secondname=("Thapa");
+        String Fullname= firstname + Secondname;
+    System.out.println( Fullname);
+    }
+    }
