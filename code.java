bd290171d517058c73c8e40576ca9deff942da17
thapajr2007-debug <thapajr2007@gmail.com>
2026-03-28 14:54:19 +0545

    Student Report using Java

--- code.java
+++ code.java
@@ -1,8 +1,15 @@
 public class code {
     public static void main(String[] args){
-        String firstname= ("Rajan");
-        String Secondname=("Thapa");
-        String Fullname= firstname + Secondname;
-    System.out.println( Fullname);
+        String Studentfirstname= ("Rajan");
+        String StudentSecondname=("Thapa");
+        String StudentFullname= Studentfirstname + StudentSecondname;
+        int StudentID = 1;
+        int StudentAge = 19;
+        char StudentGrade = 'A';
+
+    System.out.println("StudentFullname: " + StudentFullname);
+    System.out.println("StudentID: " + StudentID);
+    System.out.println("StudentAge: " + StudentAge);
+    System.out.println("StudentGrade: " + StudentGrade);
     }
     }
